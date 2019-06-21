0922cf76b88f7ad1e5b90a745785df21bdc37042
oneicecream <w15886761501@163.com>
2019-06-21 21:20:15 +0800

    发送请求获取初始化验证码的参数

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -9,19 +9,24 @@
         <el-input v-model="form.mobile" placeholder="手机号"></el-input>
       </el-form-item>
       <el-form-item>
-          <!-- el-col 栅格布局，一共 24 列，:span 用来指定占用的大小，:offset 用来指定偏移量 -->
-        <el-col :span='14'>
+        <!-- el-col 栅格布局，一共 24 列，:span 用来指定占用的大小，:offset 用来指定偏移量 -->
+        <el-col :span="14">
           <el-input v-model="form.code" placeholder="验证码"></el-input>
         </el-col>
-        <el-col :offset='1' :span='9'>
-          <el-button type="primary" @click="onSubmit">获取验证码</el-button>
+        <el-col :offset="1" :span="9">
+          <el-button type="primary" @click="handleSendCode">获取验证码</el-button>
         </el-col>
+      </el-form-item>
+      <el-form-item class="login-denglu">
+        <el-button type="primary" @click="onSubmit">登录</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'AppLogin',
   data () {
@@ -32,10 +37,19 @@ export default {
       }
     }
   },
-
   methods: {
     onSubmit () {
       console.log('submit!')
+    },
+
+    handleSendCode () {
+      const mobile = this.form.mobile
+      axios({
+        method: 'GET',
+        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
+      }).then(res => {
+        console.log(res.data)
+      })
     }
   }
 }
@@ -54,6 +68,12 @@ export default {
     box-sizing: border-box;
     padding: 20px;
     border-radius: 20px;
+    .login-denglu {
+        width: 100%;
+        .el-button {
+            width: 100%;
+        }
+    }
     p {
       width: 100%;
       text-align: center;
