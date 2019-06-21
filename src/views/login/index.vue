6fe6e8b8f537389008df4ae80940d8f1f95074af
oneicecream <w15886761501@163.com>
2019-06-21 22:16:29 +0800

    成功发送验证码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -70,7 +70,22 @@ export default {
               })
               .onSuccess(function () {
                 // your code
-                console.log(captchaObj.getValidate())
+                // console.log(captchaObj.getValidate())
+                const { geetest_challenge: challenge,
+                  geetest_validate: validate,
+                  geetest_seccode: seccode
+                } = captchaObj.getValidate()
+                axios({
+                  method: 'GET',
+                  url: `http://ttapi.research.itcast.cn/mp/v1_0/sms/codes/${mobile}`,
+                  params: {
+                    challenge,
+                    validate,
+                    seccode
+                  }
+                }).then(res => {
+                  console.log(res.data)
+                })
               })
               .onError(function () {
                 // your code
