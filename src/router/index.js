330f4b12d81bbba1673deb7e1edce6d7fe8847c9
oneicecream <w15886761501@163.com>
2019-06-23 22:12:56 +0800

    Layout布局组件-Container布局容器.

--- src/router/index.js
+++ src/router/index.js
@@ -5,16 +5,21 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      name: 'home',
-      path: '/',
-      component: () => import('@/views/home/index.vue')
-    },
+    // {
+    //   name: 'home',
+    //   path: '/',
+    //   component: () => import('@/views/home/index.vue')
+    // },
     {
       name: 'login',
       path: '/login',
       // 路由懒加载
       component: () => import('@/views/login/index.vue')
+    },
+    {
+      name: 'layout',
+      path: '/',
+      component: () => import('@/views/layout/index.vue')
     }
   ]
 })
---
+++ src/views/layout/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <!-- <div class="layout-wrap">
+        <AppAside></AppAside>
+        <AppHeader></AppHeader>
+  </div>-->
+  <el-container>
+    <el-aside width="200px">
+      <AppAside/>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <AppHeader/>
+      </el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import AppAside from './components/AppAside'
+import AppHeader from './components/AppHeader'
+
+export default {
+  name: 'AppLayout',
+  data () {
+    return {}
+  },
+  components: {
+    AppAside,
+    AppHeader
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.el-container {
+  height: 100%;
+}
+
+.el-aside {
+  background-color: #343a46;
+}
+
+.el-header {
+  background: #aec0d1;
+}
+
+.el-main {
+  background-color: #e8eff4;
+}
+</style>
