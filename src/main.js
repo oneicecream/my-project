2523e31e24a90b2ac8f3047fcc41046a594f6193
oneicecream <w15886761501@163.com>
2019-06-28 15:43:41 +0800

    响应拦截器-统一处理401无效token

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'nprogress/nprogress.css'
 import axios from 'axios'
-import { getUser } from '@/utils/auth'
+import { getUser, removeUser } from '@/utils/auth'
 
 import router from './router'
 
@@ -17,7 +17,7 @@ import './styles/index.css'
 axios.defaults.baseURL = 'http://toutiao.course.itcast.cn/mp/v1_0/'
 
 // axios 请求拦截器： axios 发出去的请求会先经过这里
-axios.interceptors.request.use(config => {
+axios.interceptors.request.use(config => { // >=200 && <400的状态码会进入这里
   const user = getUser()
 
   // confit 是本次请求相关的配置对象
@@ -28,7 +28,7 @@ axios.interceptors.request.use(config => {
   // return config 是允许请求发送的开关
   // 我们可以在这之前进行一些业务逻辑操作
   return config
-}, error => {
+}, error => { // >=400的状态码会进入这里
   return Promise.reject(error)
 })
 
@@ -43,6 +43,16 @@ axios.interceptors.response.use(response => {
     return response.data
   }
 }, error => {
+  // 如果用户 token 无效，让其跳转到登录页面
+  if (error.response.status === 401) {
+    // 清楚本地储存中的无效 token 的用户信息
+    removeUser()
+
+    // 跳转到用户登录页面
+    router.push({
+      name: 'login'
+    })
+  }
   return Promise.reject(error)
 })
 
